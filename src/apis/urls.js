22588d77249b0468b37c98c46f952cb46f8fdcff
Supreet Purohit Prakash <supreet.official2020@gmail.com>
2023-06-28 23:50:38 +0545

    first step styling fixes

--- src/apis/urls.js
+++ src/apis/urls.js
@@ -1,4 +1,4 @@
-BACKEND_URL = "http://localhost:8000"
+BACKEND_URL = "https://copyai.pythonanywhere.com"
 export var BACKEND_URL;
 
 export const BACK_API_LOGIN_URL = "/v1/accounts/accounts_data/login_using_token/";
