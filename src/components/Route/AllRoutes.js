a9d7b5d5da1f8f2612784c6290c510467e708954
Supreet Purohit Prakash <supreet.official2020@gmail.com>
2023-07-15 18:36:42 +0545

    chat css

--- src/components/Route/AllRoutes.js
+++ src/components/Route/AllRoutes.js
@@ -10,7 +10,7 @@ import { useSelector, useDispatch } from "react-redux";
 
 import { _save_token_ } from "../../features/AuthenticationToken";
 import Profile from "../Profile/Profile";
-import Chat from "../pages/chat/Chat";
+import Chat from "../pages/Chat/Chat";
 import Template from "../pages/Template/Template";
 import SingleTemplate from "../pages/Template/SingleTemplate";
 import EditDocuments from "../pages/Template/Document/EditDocuments";
@@ -67,8 +67,11 @@ const AllRoutes = ({ _TOKEN_FOR_VALIDATION_NAVBAR_ }) => {
                 />
               }
             />
-            
-            <Route path="/template_data/:document_id" element={<EditDocuments  AUTH_TOKEN={TOKEN} />} />
+
+            <Route
+              path="/template_data/:document_id"
+              element={<EditDocuments AUTH_TOKEN={TOKEN} />}
+            />
 
             <Route path="/*" element={<Home />} />
             <Route path="/Projects" element={<Projects />} />
