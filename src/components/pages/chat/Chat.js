eaad93da19d79ea4347f515376a076a0fcf96bd4
Roshankc682 <kcroshan682@gmail.com>
2023-08-26 16:03:02 +0545

    urls

--- src/components/pages/chat/Chat.js
+++ src/components/pages/chat/Chat.js
@@ -1497,7 +1497,48 @@ const Chat = ({ AUTH_TOKEN }) => {
                     ?
                         <button
                           className="absolute bottom-4 right-4 w-6 h-6"
-                         onClick={handleStopTyping}>Stop Typing</button>
+                         onClick={handleStopTyping}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="30"
+                            height="30"
+                            viewBox="0 0 24 24"
+                          >
+                            <circle cx="18" cy="12" r="0" fill="currentColor">
+                              <animate
+                                attributeName="r"
+                                begin=".67"
+                                calcMode="spline"
+                                dur="1.5s"
+                                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                                repeatCount="indefinite"
+                                values="0;2;0;0"
+                              />
+                            </circle>
+                            <circle cx="12" cy="12" r="0" fill="currentColor">
+                              <animate
+                                attributeName="r"
+                                begin=".33"
+                                calcMode="spline"
+                                dur="1.5s"
+                                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                                repeatCount="indefinite"
+                                values="0;2;0;0"
+                              />
+                            </circle>
+                            <circle cx="6" cy="12" r="0" fill="currentColor">
+                              <animate
+                                attributeName="r"
+                                begin="0"
+                                calcMode="spline"
+                                dur="1.5s"
+                                keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                                repeatCount="indefinite"
+                                values="0;2;0;0"
+                              />
+                            </circle>
+                          </svg>
+                         </button>
                     :
                       <>
                       <button
