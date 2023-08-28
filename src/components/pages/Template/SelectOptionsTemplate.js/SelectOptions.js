e03c557d149a2089b692a484a043801e7df92ed3
Roshankc682 <kcroshan682@gmail.com>
2023-08-29 00:00:56 +0545

    added draft pending etc .. api integration and the plan checked status

--- src/components/pages/Template/SelectOptionsTemplate.js/SelectOptions.js
+++ src/components/pages/Template/SelectOptionsTemplate.js/SelectOptions.js
@@ -1,68 +1,67 @@
 import React, { useEffect, useState } from "react";
-import { BACKEND_URL,BACK_END_API_DOCUMENTS } from "../../../../apis/urls";
-import { patchData, postData } from "../../../../apis/apiService";
+import { BACKEND_URL, BACK_END_API_DOCUMENTS } from "../../../../apis/urls";
+import { patchData } from "../../../../apis/apiService";
+
 const SelectOptionsTemplate = (props) => {
-
-
   const [selectedOption, setSelectedOption] = useState("");
   const [colorIt, setcolorIt] = useState("bg-black");
-  
-  const [options, setoptions] = useState([
+
+  const options = [
     { text: "Done", color: "bg-green-800" },
     { text: "Working", color: "bg-green-600" },
     { text: "Publish", color: "bg-green-800" },
     { text: "Draft", color: "bg-blue-400" },
     { text: "Urgent", color: "bg-red-700" },
     { text: "Pending", color: "bg-red-500" },
-  ]);
+  ];
 
-
-  const update_status = async(id,options,TOKEN) =>{
+  const updateStatus = async (id, option, TOKEN) => {
     const formData = {
-      status:options
-    }
-    const resp = await patchData(formData,BACKEND_URL+BACK_END_API_DOCUMENTS+"/"+id+"/",TOKEN)
-  }
-  
+      status: option,
+    };
+    const resp = await patchData(
+      formData,
+      BACKEND_URL + BACK_END_API_DOCUMENTS + "/" + id + "/",
+      TOKEN
+    );
+  };
+
   useEffect(() => {
+    // Find the selected option object
     const selectedText = options.find((option) => option.text === selectedOption);
+
     if (selectedText) {
-      setcolorIt(selectedText.color)
-      update_status(props.id,selectedOption,props.TOKEN)
-      
+      setcolorIt(selectedText.color);
+      updateStatus(props.id, selectedOption, props.TOKEN);
     }
   }, [selectedOption]);
 
+  useEffect(() => {
+    // Find the selected option object by comparing with props
+    const selectedText = options.find(
+      (option) => option.text === props.options_selected.status
+    );
 
-  useEffect(()=>{
-    
-    const selectedText = options.find((option) => option.text === props.options_selected.status);
     if (selectedText) {
-      setcolorIt(selectedText.color)
-      setSelectedOption(selectedText.text)
-      const filteredOptions = options.filter(option => option.text !== props.options_selected.status);
-      setoptions(filteredOptions)
+      setcolorIt(selectedText.color);
+      setSelectedOption(selectedText.text);
     }
-
-  },[])
+  }, [props.options_selected.status]); // Trigger this effect when status changes in props
 
   return (
     <>
-        <select
-          className={`${colorIt} text-white rounded-full text-[12px] w-[70px] h-[25px] font-bold  text-center appearance-none`}
-          onChange={(e) => setSelectedOption(e.target.value)}
-        >
-          <option value="">{selectedOption}</option>
-          {options.map((option) => (
-            <option
-              key={option.text}
-              value={option.text}
-            >
-              {option.text}
-            </option>
-          ))}
-        </select>
-        </>
+      <select
+        className={`${colorIt} text-white rounded-full text-[12px] w-[70px] h-[25px] font-bold  text-center appearance-none`}
+        value={selectedOption} // Use value to control selected option
+        onChange={(e) => setSelectedOption(e.target.value)}
+      >
+        {options.map((option) => (
+          <option key={option.text} value={option.text}>
+            {option.text}
+          </option>
+        ))}
+      </select>
+    </>
   );
 };
 
